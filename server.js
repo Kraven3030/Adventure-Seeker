04b53a14cfda8f141bf2446f716e30ea8a3c52b6
BlakeR <blakereed3030@gmail.com>
2022-11-28 18:06:38 -0800

    update

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 // +-+-+-+-+-+-+-+-+-+-+-+-+
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT
 const methodOverride = require('method-override');
-require('dotenv').config()
 
 
 // access models
